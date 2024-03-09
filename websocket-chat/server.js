9c74ca2e39ca49fe4ef2d0167981ded958fbd185
Suraj Yadav <111379728+suraj826580@users.noreply.github.com>
2024-03-09 09:55:44 +0530

    update websocket-chat/index.html and server.js

--- websocket-chat/server.js
+++ websocket-chat/server.js
@@ -3,8 +3,15 @@ const { WebSocketServer } = require("ws");
 const wss = new WebSocketServer({ port: 8080 });
 
 wss.on("connection", (socket) => {
-  console.log("socket");
-});
+  console.log("Got a New Connection");
+  socket.on("message", (message) => {
+    // console.log(wss.clients);
+    // console.log(JSON.parse(message));
 
+    wss.clients.forEach((clients) => {
+      console.log(clients.readyState);
+    });
+  });
+});
 
 // today commit
